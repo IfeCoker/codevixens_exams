7a4646b281e6b6290a47ccb0c8c172f5d4a022bc
Ifeoluwa <ifeoluwaadeleke3008@gmail.com>
2023-03-24 22:24:20 +0100

    exam_one

---
+++ src/Exam.js
@@ -0,0 +1,15 @@
+
+const items = [
+    {id: "1",name: "", checkbox},
+    {id: "2",name: "", checkbox},
+    {id: "3",name: "", checkbox},
+]
+
+function Item({name, checkbox}) {
+    
+}
+export function RenderList({name, id}) {
+    return(
+
+    )
+}
